44c3870a8f1e279f018d336db17ff3583d99a2d8
Stiiii <48644228+Stiiii@users.noreply.github.com>
2023-03-27 14:08:20 +0200

    TrooperEditor
    trooper editor look

---
+++ Workshop_03_Army_Editor/TrooperEditor.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Workshop_03_Army_Editor
+{
+	/// <summary>
+	/// Interaction logic for TrooperEditor.xaml
+	/// </summary>
+	public partial class TrooperEditor : Window
+	{
+		public TrooperEditor()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			foreach (var item in stack.Children)
+			{
+				if (item is TextBox tb)
+				{
+					tb.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+				}
+			}
+			this.DialogResult = true;
+		}
+	}
+}
